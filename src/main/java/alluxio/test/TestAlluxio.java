7aca5e00479571f807f8a784f877218d11eb7ffa
maobaolong <maobaolong@139.com>
2017-02-09 15:17:54 +0800

    add assembler

--- src/main/java/alluxio/test/TestAlluxio.java
+++ src/main/java/alluxio/test/TestAlluxio.java
@@ -12,6 +12,8 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * alluxio.test <br>
@@ -27,15 +29,19 @@ import java.io.IOException;
 public class TestAlluxio {
   private static final String DEFAULT_PATH =
       "alluxio://172.16.150.101:19998/ns2/user/maobaolong/mbltest/mbltest.txt";
+  String alluxioFilePath = DEFAULT_PATH;
+  boolean keepOpen = false;
 
   public static void main(String[] args) throws IOException, InterruptedException {
-    String alluxioFilePath = DEFAULT_PATH;
+    final TestAlluxio ta = new TestAlluxio();
+
     boolean go = false;
 
     Options opts = new Options();
     opts.addOption("h", false, "help");
     opts.addOption("path", true, "alluxio file path. default " + DEFAULT_PATH);
     opts.addOption("go", false, "after everything done, terminate.");
+    opts.addOption("keepOpen", false, "keey open.");
     BasicParser parser = new BasicParser();
     CommandLine cl;
     try {
@@ -47,7 +53,7 @@ public class TestAlluxio {
           return;
         } else {
           if (cl.hasOption("path")) {
-            alluxioFilePath = cl.getOptionValue("path");
+            ta.alluxioFilePath = cl.getOptionValue("path");
           }
           go = cl.hasOption("go");
         }
@@ -58,15 +64,33 @@ public class TestAlluxio {
       e.printStackTrace();
       return;
     }
-    System.out.println("path : " + alluxioFilePath);
+    System.out.println("path : " + ta.alluxioFilePath);
     System.out.println("go : " + go);
-
+    System.out.println("keepOpen : " + cl.hasOption("keepOpen"));
     System.out.println("start test:");
     //    AlluxioURI path = new AlluxioURI("/ns2/user/maobaolong/mbltest/mbltest.txt");
+    ExecutorService fixedThreadPool = Executors.newFixedThreadPool(4000);
     for (int i = 0; i < 4000; i++) {
       if (i % 40 == 0) {
         System.out.printf("precent %d  .\n", i / 40);
       }
+      fixedThreadPool.execute(new Runnable() {
+        @Override
+        public void run() {
+          ta.doTest();
+        }
+      });
+
+    }
+    System.out.println("test Ok!");
+    while (!go) {
+      System.out.println("I am alive!");
+      Thread.sleep(60 * 1000L);
+    }
+  }
+
+  public void doTest() {
+    try {
       Path path = new Path(alluxioFilePath);
       Configuration configuration = new Configuration();
       FileSystem fileSystem = path.getFileSystem(configuration);
@@ -74,12 +98,10 @@ public class TestAlluxio {
       long size = fileSystem.getFileStatus(path).getLen();
 //      System.out.println("size: " + size);
       FSDataInputStream inputStream = fileSystem.open(path);
-      inputStream.close();
-    }
-
-    while (!go) {
-      Thread.sleep(60 * 1000L);
-      System.out.println("I am alive!");
+      if (!keepOpen)
+        inputStream.close();
+    }catch(IOException e){
+      e.printStackTrace();
     }
   }
 }
